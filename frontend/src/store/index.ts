65dd6de112cd4b56004998abf6f507176f480213
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-07-22 02:04:48 +0800

    faas api

--- frontend/src/store/index.ts
+++ frontend/src/store/index.ts
@@ -2,7 +2,7 @@ import {FileApi} from "../api";
 import {_fileInfoStore} from "./fileInfo.ts";
 import {_configStore} from "./config.ts";
 
-export  const API_BASE_PATH = "http://127.0.0.1:8081/api";
+export  const API_BASE_PATH = "https://sigma.kevinzonda.com/funcs/file-share/api";
 export const FileAPI = new FileApi()
 export const ConfigStore = new _configStore()
 export const FileInfoStore = new _fileInfoStore();
