742d9d60e5edab41f2880b23e52a1e61eaad264e
KevinZønda <33132228+KevinZonda@users.noreply.github.com>
2023-07-22 01:45:56 +0800

    auth

--- controllers/router.go
+++ controllers/router.go
@@ -1,14 +1,35 @@
 package controllers
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/KevinZonda/file-sharer/shared"
+	"github.com/gin-gonic/gin"
+)
 
 func Router(e *gin.Engine) {
 	e.GET("/ping", Ping)
 
 	apiGroup := e.Group("/api")
 
-	apiGroup.POST("/file/upload", fileUpload)
+	apiGroup.POST("/file/upload", auth, fileUpload)
 	apiGroup.POST("/file/download", fileDownload)
 	apiGroup.GET("/file/:id", getFileDownload)
 	apiGroup.POST("/file/info", fileInfo)
+}
+
+func auth(c *gin.Context) {
+	if shared.GetConfig().Auth.AllowAll {
+		c.Next()
+		return
+	}
+	h := c.GetHeader("Authorisation")
+
+	tokens := shared.GetConfig().Auth.Keys
+	for _, token := range tokens {
+		if token == h {
+			c.Next()
+			return
+		}
+	}
+	c.JSON(401, gin.H{"error": "Unauthorised"})
+	c.Abort()
 }
--- models/config.go
+++ models/config.go
@@ -4,4 +4,8 @@ type ConfigModel struct {
 	ListenAddr   string `json:"listen_addr"`
 	MaxSizeInKiB int64  `json:"max_size"`
 	UploadDir    string `json:"upload_dir"`
+	Auth         struct {
+		AllowAll bool     `json:"allow_all"`
+		Keys     []string `json:"keys"`
+	} `json:"auth"`
 }
